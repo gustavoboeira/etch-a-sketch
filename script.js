6a9f3d5f3466b9acc119f89f3c2b7ec860f073df
Gustavo Boeira <gustavoboeirasilva@gmail.com>
2020-07-18 14:07:39 -0300

    deseinho

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-cosnt containr = document.querySelector('#container');
+let container = document.querySelector('#container');
+let i;
+let j;
+
+
+for(i = 0; i < 256; i++){
+        let content = document.createElement('div');
+        content.classList.add('quadradinho');
+        container.appendChild(content);
+}
+
+
+let quadradinho = document.querySelector(".quadradinho");
+
+quadradinho.addEventListener("mouseover", function(e){
+        e.currentTarget.style.color = 'black';
+});
+
+console.log(container)
